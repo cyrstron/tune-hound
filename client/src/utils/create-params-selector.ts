da996cae548488e1d5029aa6ac27210df809498a
Kyryl Stronko <kirstronko@gmail.com>
2021-03-28 12:09:58 +0300

    createParamsSelector

--- client/src/utils/create-params-selector.ts
+++ client/src/utils/create-params-selector.ts
@@ -2,26 +2,45 @@ import { AppSelector, AppSelectorCreator, AppState } from '@app/state';
 import { createSelector, ParametricSelector } from 'reselect';
 import { memoize, MemoizeOptions } from './memoize';
 
-export type P
+export interface ParamsSelectorOptions {
+  cacheSize?: number;
+}
 
-export function createParamsSelector<TFunc extends AppSelectorCreator<TFunc>>(
-  ...args: [...ParametricSelector<AppState, unknown, unknown>[], TFunc]
-): ReturnType<typeof createSelector>;
-export function createParamsSelector<TFunc extends AppSelectorCreator<TFunc>>(
-  ...args: [...ParametricSelector<AppState, unknown, unknown>[], TFunc, MemoizeOptions]
-): ReturnType<typeof createSelector> {
-  const [lastArg] = args.pop();
+// export function createParamsSelector<TFunc extends AppSelectorCreator<TFunc>>(
+//   ...args: [...ParametricSelector<AppState, unknown, unknown>[], TFunc]
+// ): ReturnType<typeof createSelector>;
+// export function createParamsSelector<TFunc extends AppSelectorCreator<TFunc>>(
+//   ...args: [...ParametricSelector<AppState, unknown, unknown>[], TFunc, MemoizeOptions]
+// ): ReturnType<typeof createSelector> {
+//   const [lastArg] = args.pop();
 
-  let options = {};
-  let selectorCreator: TFunc;
+//   let options = {};
+//   let selectorCreator: TFunc;
 
-  if (typeof lastArg === 'function') {
-    selectorCreator = lastArg;
-  } else {
-    options = lastArg;
+//   if (typeof lastArg === 'function') {
+//     selectorCreator = lastArg;
+//   } else {
+//     options = lastArg;
+//   }
+
+//   const memoized = memoize(selectorCreator);
+
+//   return createSelector(...params, memoized);
+// }
+
+export function createParamsSelector(
+  ...args:
+    | Parameters<typeof createSelector>
+    | [...Parameters<typeof createSelector>, ParamsSelectorOptions]
+) {
+  const lastArg = args.pop();
+
+  let options: ParamsSelectorOptions = {};
+  if (typeof lastArg === 'object') {
+    options = lastArg as ParamsSelectorOptions;
   }
 
-  const memoized = memoize(selectorCreator);
+  const selector = memoize((...params: any[]) => createSelector());
 
-  return createSelector(...params, memoized);
+  function paramsSelector(state: AppState, ...params: any[]) {}
 }
